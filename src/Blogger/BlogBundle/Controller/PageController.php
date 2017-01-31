63ec5f775604ec8db7c088cff36a245571bca3ae
Dmytro <you@example.com>
2017-01-31 16:35:50 +0200

    add contact form

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -5,6 +5,10 @@ namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
 class PageController extends Controller
 {
     public function indexAction()
@@ -15,5 +19,38 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('BlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blog.emails.contact_email'))
+                    ->setBody($this->renderView('BlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+
+
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BlogBundle_contact'));
+
+            }
+        }
+
+        return $this->render('BlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
---
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -0,0 +1,112 @@
+<?php
+
+// src/Blogger/BlogBundle/Entity/Enquiry.php
+
+namespace Blogger\BlogBundle\Entity;
+
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
+
+class Enquiry
+{
+    /**
+     * @var
+     */
+    protected $name;
+
+    /**
+     * @var
+     */
+    protected $email;
+
+    /**
+     * @var
+     */
+    protected $subject;
+
+    /**
+     * @var
+     */
+    protected $body;
+
+    /**
+     * @return mixed
+     */
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+        $metadata->addPropertyConstraint('email', new Email());
+        $metadata->addPropertyConstraint('subject',  new Length(array(
+            'max'        => 50
+
+        )));
+        $metadata->addPropertyConstraint('body', new Length(array(
+            'min'        => 50
+        )));
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+}
