8f01959fffa44b174a9402d72f1552adb32b40b0
Dmytro <you@example.com>
2017-01-31 12:40:03 +0200

    fix urls, add about page

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -11,4 +11,9 @@ class PageController extends Controller
     {
         return $this->render('BlogBundle:Page:index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('BlogBundle:Page:about.html.twig');
+    }
 }
